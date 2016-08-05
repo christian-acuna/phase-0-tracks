232c661075c592ec1a5bbfbae8047d057ea85717
Christian Acuna <cacuna0828@gmail.com>
2016-08-05 10:12:41 +0800

    Add age_test method

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -15,6 +15,15 @@ def true_or_false(item)
   item
 end
 
+def age_test(age, year_born)
+  current_year = 2016
+  if current_year - year_born == age
+    false
+  else
+    true
+  end
+end
+
 puts 'Hello'
 puts 'Welcome to our company. We are going to ask you a few questions.'
 puts 'What is you name?'
@@ -23,8 +32,10 @@ puts 'How old are you?'
 age = gets.chomp.to_i
 puts 'Great, what year were you born?'
 year = gets.chomp.to_i
-puts 'Our company cafeteria serves garlic bread. Should we order some for you? (Y/N)'
+fail_age_test = age_test(age, year)
+puts 'Our company cafeteria serves garlic bread.
+Should we order some for you? (Y/N)'
 bread = true_or_false(bread)
 puts 'Would you like to enroll in the company’s health insurance?'
 health_insurance = true_or_false(health_insurance)
-puts "break = #{bread} and health = #{health_insurance}"
+puts "bread = #{bread} and health = #{health_insurance}"
