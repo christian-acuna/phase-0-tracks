440f3f05fa939c42398c92562cfbc47a585419fd
Christian Acuna <cacuna0828@gmail.com>
2016-08-10 10:00:09 +0800

    Create add_to_array method

--- ruby/arrays_drill.rb
+++ ruby/arrays_drill.rb
@@ -3,9 +3,22 @@ def build_array(one, two, three)
     [one, two, three]
 end
 
+# add a method that takes an array and item as parameters and returns the array
+# with the item added
 
+def add_to_array(array, item)
+    array << item
+end
+puts "build_array"
 p build_array(1, 2, 3)
+
 puts "------------"
+puts "add_to_array"
+p add_to_array([1, 2, 3], 4)
+p add_to_array(%w{one two three}, "four")
+pets = %w{dog cat}
+p add_to_array(pets, 'hamster')
+puts "-------------"
 
 # Create array and add var to array
 array = []
@@ -27,7 +40,7 @@ array.insert(2, "string")
 p array
 
 #5. Remove the first item of the array
-p "-------"
+puts "-------"
  array.shift
 p array
 # p array.drop(1)
