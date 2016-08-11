b27720ea90cade9a5622f3a91a6a2e198744e498
Christian Acuna <cacuna0828@gmail.com>
2016-08-11 14:17:12 +0800

    add pseudocode for challenge

---
+++ ruby/hashes.rb
@@ -0,0 +1,11 @@
+# GOAL: allow an interior designer to enter the details of a given client
+
+# 1. Greet designer/user
+# 2. Prompt the desinger/user for information to enter
+# 3. Print hash back out to screen after designer has answered all of
+# the questions
+# 4. Give the desinger to opporunity to update a key once.
+# 5. If desinger says "none", skip step 4
+# => if designer says :decor_theme for emaple, update :decore_theme key
+# 6. Print the lastest version of the hash
+# 7. Exit the program
