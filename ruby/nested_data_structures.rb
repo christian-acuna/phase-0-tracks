644a24b2ede0298a00ef025b8c733bf8577253c0
Christian Acuna <cacuna0828@gmail.com>
2016-08-14 13:16:03 +0800

    add musuems nested data structure

--- ruby/nested_data_structures.rb
+++ ruby/nested_data_structures.rb
@@ -1,1 +1,55 @@
 # Goal: design and build a nested data structure
+musuems = {
+  louvre: {
+    pretty_name: 'Louvre',
+    city: 'Paris',
+    country: 'France',
+    gallery_space: 72735,
+    year_established: 1792,
+    collections: [
+      'Egyptian antiquities',
+      'Near Eastern antiquities',
+      'Greek, Etruscan, and Roman',
+      'Islamic art',
+      'Sculpture',
+      'Decorative arts',
+      'Painting',
+      'Prints and drawings'
+    ]
+  },
+  tokyo_national_musuem: {
+    pretty_name: 'Tokyo National Museum',
+    city: 'Tokyo',
+    country: 'Japan',
+    gallery_space: 38000,
+    year_established: 1872,
+    exhibition_buildings: [
+      'Honkan (Japanese Gallery)',
+      'Tōyōkan (Asian Gallery)',
+      'Hyōkeikan',
+      'Heiseikan',
+      'Sculpture',
+      'Hōryū-ji Hōmotsukan (The Gallery of Hōryū-ji Treasures)'
+    ]
+  },
+  art_institute_chicago: {
+    pretty_name: 'Art Institute of Chicago',
+    city: 'Chicago',
+    country: 'United States',
+    gallery_space: 26000,
+    year_established: 1870,
+    collections: [
+      'African Art and Indian Art of the Americas',
+      'American Art',
+      'Ancient and Byzantine',
+      'Architecture and Design',
+      'Asian Art',
+      'European Decorative Arts',
+      'European Painting and Sculpture',
+      'Modern and Contemporary Art',
+      'Photography',
+      'Prints and Drawings',
+      'Textiles'
+    ]
+  }
+}
