5b476e00776970027985a8938ced93f5c8f9912d
Christian Acuna <cacuna0828@gmail.com>
2016-08-16 13:09:35 +0800

    Design Puppy Class

---
+++ ruby/puppy.rb
@@ -0,0 +1,24 @@
+# BREED ------------------------
+# Labrador Retriever
+#
+# CHARACTERISTICS ----------------
+# Eyelash length: varies
+# Cute: true
+# Friendly: yes
+# Patient: true
+# Eye count: 2
+# Name: varies
+# Color: varies
+# Life span: 13 years
+# Common name: Lab
+#
+# BEHAVIOR -----------------------
+# Bark
+# Jump
+# Sleep
+# Eat
+# Fight
+# Play_dead
+# Defend
+# Hunt
+# Play
