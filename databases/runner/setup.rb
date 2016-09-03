838bfe1e39e9f1040002b0e9a6e584661ddc7c60
Christian Acuna <cacuna0828@gmail.com>
2016-09-03 09:42:10 +0800

    create seed_runs and generate 100 runs

--- databases/runner/setup.rb
+++ databases/runner/setup.rb
@@ -25,13 +25,13 @@ module RunTrackerDB
 
       CREATE TABLE runs(
         id INTEGER PRIMARY KEY AUTOINCREMENT,
-        duration VARCHAR(16),
-        calories REAL,
+        duration INT,
+        calories INT,
         location VARCHAR(64),
+        user_id INT,
         ran_at DATETIME,
         created_at DATETIME NOT NULL,
         updated_at DATETIME NOT NULL,
-        user_id INT,
         FOREIGN KEY(user_id) REFERENCES users(id)
       );
     SQL
@@ -46,6 +46,21 @@ module RunTrackerDB
     SQL
     $db.execute(seed_users_sql, *values)
   end
+
+  def self.seed_runs(*values)
+    # https://stackoverflow.com/questions/14202944/sqlite-select-random-datetime-in-range
+    random_date = <<-DATE
+    datetime(strftime('%s', '2000-01-01 00:00:00') +
+                abs(random() % (strftime('%s', '2000-01-31 23:59:59') -
+                                strftime('%s', '2000-01-01 00:00:00'))
+                   ), 'unixepoch')
+                   DATE
+    seed_runs_sql = <<-SQL
+      INSERT INTO runs
+      VALUES (NULL, ?, ?, ?, ?, #{random_date}, DATETIME('now'), DATETIME('now'));
+    SQL
+    $db.execute(seed_runs_sql, values)
+  end
 end
 
 RunTrackerDB.setup
@@ -54,4 +69,11 @@ RunTrackerDB.setup
 end
 100.times do
   RunTrackerDB.seed_users(Faker::Name.first_name, Faker::Name.last_name, 'F', Faker::Internet.email)
+end
+
+100.times do
+  location = "#{Faker::Address.city}, #{Faker::Address.state_abbr}"
+  RunTrackerDB.seed_runs(Faker::Number.number(2), Faker::Number.between(1, 700),
+                         location,
+                         Faker::Number.between(1, 200))
 end
