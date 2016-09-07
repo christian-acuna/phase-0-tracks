afe45ef9eb82523206f718cf9ee14c52c9ffa881
Christian Acuna <cacuna0828@gmail.com>
2016-09-07 11:04:37 +0800

    add event listeners to div elements

--- web_dev/client_side_js/script.js
+++ web_dev/client_side_js/script.js
@@ -7,8 +7,22 @@ el.innerHTML = '<h2>FOUR</h2>';
 el.onmouseover = console.log('log');
 el.style.color = 'red';
 
+var centerText = function () {
+  this.style.textAlign = "center";
+  console.log('hello');
+};
+
 var list = document.querySelectorAll('div');
 console.log(list);
 for (var item of list) {
   item.style.fontSize = '25px';
+  item.addEventListener("mouseover", centerText);
 }
+
+// var alertOnClick = ;
+
+document.getElementById('child-3').addEventListener("click", function () {
+  alert('Hi There!!!!!!!!!');
+});
+
+// document.getElementById('child-2').addEventListener("mouseover", centerText);
